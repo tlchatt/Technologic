56d8deb0228215cc9b081b2e48cb8bc34980296e
Gellert Grindelwald <greggorywiley@tlchatt.com>
2017-07-29 18:40:15 -0400

    Home page text

--- header.php
+++ header.php
@@ -15,7 +15,8 @@
 <body>
   <nav role="navigation">
     <div class="nav-wrapper container">
-        <a id="logo-container" href="http://tlchatt.com/" class="brand-logo"><img class="img-responsive center" src="./img/technologic_square_white_logo.png" alt="Technologic Chattanooga"></a>
+        <a id="logo-container" href="http://tlchatt.com/" class="brand-logo hide-on-med-and-up"><img class="img-responsive center" src="./img/technologic_square_white_logo.png" alt="Technologic Chattanooga"></a>
+        <a id="logo-container" href="http://tlchatt.com/" class="brand-logo hide-on-small-only"><img class="img-responsive center" src="./img/Technologic_logo_white.png" alt="Technologic Chattanooga"></a>
       <ul class="right hide-on-med-and-down">
         <li><a href="/online-advertising.php">Advertise Online</a></li>
         <li><a href="/search-engine-optimization.php">Optimize for Search and Social</a></li>
--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   <div class="section" id="index-banner">
     <div class="row center container section">
       <h1 class="header center">Digital Marketing, Web Design and Search Engine Optimization.</h1>
-      <h3 class="header col s12 light">Equiping business for success through online advertising, search engine optimization and web design.</h2>
+      <h5 class="header col s12 light">Welcome to the Technologic, a digital marketing and web design firm based out of Chattanooga Tennessee, providing service to clients locally and nationally. Our mission is to equip clients for success online. Please explore the website and learn more about our digital marketing services.</h2>
     </div>
 
 
