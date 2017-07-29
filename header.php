264ee87e228ff25cc56cb2121fa744741a25c477
Gellert Grindelwald <greggorywiley@tlchatt.com>
2017-07-29 18:53:57 -0400

    Merge branch 'GW' of https://github.com/tlchatt/Technologic into GW

--- header.php
+++ header.php
@@ -15,16 +15,17 @@
 <body>
   <nav role="navigation">
     <div class="nav-wrapper container">
+
         <a id="logo-container" href="http://tlchatt.com/" class="brand-logo hide-on-med-and-up"><img class="img-responsive center" src="./img/technologic_square_white_logo.png" alt="Technologic Chattanooga"></a>
         <a id="logo-container" href="http://tlchatt.com/" class="brand-logo hide-on-small-only"><img class="img-responsive center" src="./img/Technologic_logo_white.png" alt="Technologic Chattanooga"></a>
       <ul class="right hide-on-med-and-down">
-        <li><a href="/online-advertising.php">Advertise Online</a></li>
-        <li><a href="/search-engine-optimization.php">Optimize for Search and Social</a></li>
+        <li><a href="online-advertising.php">Advertise Online</a></li>
+        <li><a href="search-engine-optimization.php">Optimize for Search and Social</a></li>
       </ul>
 
       <ul id="nav-mobile" class="side-nav">
-        <li><a href="/online-advertising.php">Advertise Online</a></li>
-        <li><a href="/search-engine-optimization.php">Optimize for Search and Social</a></li>
+        <li><a href="online-advertising.php">Advertise Online</a></li>
+        <li><a href="search-engine-optimization.php">Optimize for Search and Social</a></li>
       </ul>
       <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
     </div>
