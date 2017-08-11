ecafba5b850205f06481f3bf8146f6886624bda0
snewk <tehsnewk@me.com>
2017-08-11 18:00:47 -0400

    add web-design comcept page

---
+++ web-design.php
@@ -0,0 +1,76 @@
+<?php include("header.php"); ?>
+
+<div class="slider fullscreen">
+  <ul class="slides">
+    <li>
+      <img src="./img/Walnut_Street_Bridge_Chattanooga.jpg"> <!-- random image -->
+      <div class="caption center-align">
+        <h3>This is our big Tagline!</h3>
+        <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+      </div>
+    </li>
+    <li>
+      <img src="./img/Walnut_Street_Bridge_Chattanooga.jpg"> <!-- random image -->
+      <div class="caption left-align">
+        <h3>Left Aligned Caption</h3>
+        <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+      </div>
+    </li>
+    <li>
+      <img src="./img/Walnut_Street_Bridge_Chattanooga.jpg"> <!-- random image -->
+      <div class="caption right-align">
+        <h3>Right Aligned Caption</h3>
+        <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+      </div>
+    </li>
+    <li>
+      <img src="./img/Walnut_Street_Bridge_Chattanooga.jpg"> <!-- random image -->
+      <div class="caption center-align">
+        <h3>This is our big Tagline!</h3>
+        <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+      </div>
+    </li>
+  </ul>
+</div>
+
+<div class="section no-marg-bot no-pad-bot" id="index-banner">
+  <div class="container">
+    <div class="row center section">
+      <h1 class="header center">Cutting Edge Web Development</h1>
+      <h2 class="header col s12 light">High Quality Site Design and Development Services</h2>
+    </div>
+    <div class="row center">
+      <div class="col s12">
+        <p>Technologic offers custom tailored web development services. We ensure compatibility across all popular browsers and platforms. Internet users expect modern websites to load quickly regardless of whether they are using wifi or their cellular network. Sites also need to look good on any number of screen sizes from smartphone to full sized desktop.  This type of methodology is known as 'Responsive Design', and at Technologic, we build each site with responsiveness in mind.</p>
+      </div>
+    </div>
+
+    <div class="row center section">
+      <div class="col s12 m8">
+        <p>Technologic works directly on behalf of your business with the goal of producing the best return on advertising investment possible. We provide month to month obligation free service establishing long term relationships with our clients through positive results not contractual obligations.</p>
+      </div>
+
+      <div class="col s12 m4">
+        <img class="materialboxed responsive-img" src="./img/diamonds.png"/>
+      </div>
+
+      <div class="col s12 m4">
+        <img class="materialboxed responsive-img" src="./img/blueplate.png"/>
+      </div>
+
+      <div class="col s12 m8">
+        <p>Making a decisions about digital marketing for your business can be difficult. Information available though online search about digital marketing is convulted, conflicting, and aimed at getting you to sign up for any number of useless services which will waste time and money. In fact making the wrong choices can lead to various set backs in online presence and postion. Our information section is designed to help educate non marketers and amatuer marketers alike in understanding the basic principles and essential's necessary for successful online presence.</p>
+      </div>
+
+
+      <div class="col s12">
+        <p>Meet with a Technologic team member and receive a free consultation on how your company can improve its online presence through online advertising. We will look over your companies analytics and discuss best practice strategies for your businesses and explain how we can partner with your business for digital success.</p>
+      </div>
+    </div>
+    <div class="row center section">
+      <a href="#contact-modal" id="download-button" class="modal-trigger btn-large waves-effect waves-light">Get In Touch</a>
+    </div>
+  </div>
+</div>
+
+<?php include("footer.php"); ?>
