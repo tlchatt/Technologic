25ecafee023eda039b2d59cb17d006727c24d4e7
snewk <tehsnewk@me.com>
2017-08-15 13:23:18 -0400

    rearrange pictures in top image section

--- web-design.php
+++ web-design.php
@@ -5,26 +5,27 @@
   <div class="slider fullscreen">
     <ul class="slides">
       <li><img src="./img/walnut_st_bridge_filter_cropped.png"></li>
-      <li><img src="./img/snoopers.jpg"></li>
+      <li><img src="./img/raccoon.jpg"></li>
       <li><img src="./img/rooftop.jpg"></li>
       <li><img src="./img/aquarium.jpg"></li>
       <li><img src="./img/dam.jpg"></li>
       <li><img src="./img/miller.jpg"></li>
-    <div>
-      <div class="caption-wrapper left-align">
-        <h1 class="shadow2 light heading">DESIGN, SEO, & DIGITAL MARKETING FOR THE MODERN WEB</h1>
-        <h5 class="light text-lighten-3 shadow">We deliver real results for an unreal price.</h5>
+    </ul>
+      <div>
+        <div class="caption-wrapper left-align">
+          <h1 class="shadow2 light heading">DESIGN, SEO, & DIGITAL MARKETING FOR THE MODERN WEB</h1>
+          <h5 class="light text-lighten-3 shadow">We deliver real results for an unreal price.</h5>
+        </div>
+
+        <div class="logo-wrapper">
+          <img src="./img/technologic_square_white_logo.png">
+        </div>
+
+        <div id="scrollLink" href="#" class="scroll-wrapper">
+          <i class="hoverable-flat-scroll material-icons shadow2">expand_more</i>
+        </div>
       </div>
 
-      <div class="logo-wrapper">
-        <img src="./img/technologic_square_white_logo.png">
-      </div>
-
-      <div id="scrollLink" href="#" class="scroll-wrapper">
-        <i class="hoverable-flat-scroll material-icons shadow2">expand_more</i>
-      </div>
-    </div>
-        </ul>
   </div>
 </div>
 <!--end of fullscreen slider-->
@@ -54,47 +55,46 @@
 
 
 
-  <!--services section-->
-  <div style="padding: 0 30px 210px 30px;">
-    <a id="scrolldown" name="scrolldown"></a>
-    <a id="services" name="services"></a>
-    <div class="row">
-      <div class="section">
-        <h4 class="center thin">SERVICES</h4>
-      </div>
-      <div class="divider"></div>
-      <div class="col s12 l4">
-        <h5 class="light">WEB DESIGN & DEVELOPMEMNT</h5>
-        <p class="light text-lighten-1">From simple one-page sites to full wordpress customizations, we build it all.</p>
-        <!--<p>Technologic offers custom tailored web development services. We ensure compatibility across all popular browsers and platforms. Web users expect sites to load quickly regardless of whether they are using wifi or cellular. Sites also need to look good on any screen size. This is called responsive design. At Technologic, we build every site to be responsive from the ground up.</p>-->
-      </div>
-      <div class="col s12 l4">
-        <h5 class="light">SEARCH ENGINE OPTIMIZATION</h5>
-        <p class="light text-lighten-1">Rank first on search. Enough said.</p>
-      </div>
-      <div class="col s12 l4">
-        <h5 class="light">ONLINE MARKETING</h5>
-        <p class="light text-lighten-1">We deliver the higest value per dollar. Advertise across any online platform.</p>
-        <!--<p>We work directly on behalf of your business to produce the best return on advertising investment possible. We provide month to month <strong>obligation free</strong> service. We establish long-term relationships with our clients through positive results, not contractual obligations.</p>-->
-      </div>
-      <div class="col s12 l4">
-        <h5 class="light">E-COMMERCE INTEGRATION</h5>
-        <p class="light text-lighten-1">Fulfill every order, slash overhead, and manage inventory like a pro.</p>
-      </div>
-      <div class="col s12 l4">
-        <h5 class="light">FIFTH SERVICE OFFERING</h5>
-        <p class="light text-lighten-1">Brief description of the fifth service offered by Technologic.</p>
-      </div>
-      <div class="col s12 l4">
-        <h5 class="light">SIXTH SERVICE OFFERING</h5>
-        <p class="light text-lighten-1">Brief description of the sixth service offered by Technologic.</p>
-      </div>
-
+<!--services section-->
+<div style="padding: 0 30px 180px 30px;">
+  <a id="services" name="services"></a>
+  <div class="row">
+    <div class="section">
+      <h4 class="center thin">SERVICES</h4>
     </div>
+    <div class="divider"></div>
+    <div class="col s12 l4">
+      <h5 class="light">WEB DESIGN & DEVELOPMEMNT</h5>
+      <p class="light text-lighten-1">From simple one-page sites to full wordpress customizations, we build it all.</p>
+      <!--<p>Technologic offers custom tailored web development services. We ensure compatibility across all popular browsers and platforms. Web users expect sites to load quickly regardless of whether they are using wifi or cellular. Sites also need to look good on any screen size. This is called responsive design. At Technologic, we build every site to be responsive from the ground up.</p>-->
+    </div>
+    <div class="col s12 l4">
+      <h5 class="light">SEARCH ENGINE OPTIMIZATION</h5>
+      <p class="light text-lighten-1">Rank first on search. Enough said.</p>
+    </div>
+    <div class="col s12 l4">
+      <h5 class="light">ONLINE MARKETING</h5>
+      <p class="light text-lighten-1">We deliver the higest value per dollar. Advertise across any online platform.</p>
+      <!--<p>We work directly on behalf of your business to produce the best return on advertising investment possible. We provide month to month <strong>obligation free</strong> service. We establish long-term relationships with our clients through positive results, not contractual obligations.</p>-->
+    </div>
+    <div class="col s12 l4">
+      <h5 class="light">E-COMMERCE INTEGRATION</h5>
+      <p class="light text-lighten-1">Fulfill every order, slash overhead, and manage inventory like a pro.</p>
+    </div>
+    <div class="col s12 l4">
+      <h5 class="light">FIFTH SERVICE OFFERING</h5>
+      <p class="light text-lighten-1">Brief description of the fifth service offered by Technologic.</p>
+    </div>
+    <div class="col s12 l4">
+      <h5 class="light">SIXTH SERVICE OFFERING</h5>
+      <p class="light text-lighten-1">Brief description of the sixth service offered by Technologic.</p>
+    </div>
+
   </div>
+</div>
 
 
-  <!--work section-->
+<!--work section-->
 <div class="" style="background-color: #f7f7f7; padding: 0 10px 120px 10px;">
   <a id="work" name="work"></a>
   <div class="row">
@@ -116,132 +116,124 @@
   </div>
 </div>
 
-  <!--end work section-->
+<!--end work section-->
 
-  <!--company section-->
-  <div style="padding: 0 0 120px 0;">
-    <div style="padding: 0 10px 0 10px;">
-      <a id="about" name="about"></a>
-      <div class="row">
-        <div class="section">
-          <h4 class="center thin">ABOUT</h4>
-        </div>
-        <div class="col s4">
-          <img class="z-depth-3 responsive-img hoverable" src="./img/on_the_go.jpg">
-        </div>
-        <div class="col s4">
-          <img style="background-color: #7cc1f7;" class="z-depth-3 responsive-img hoverable" src="./img/Technologic_logo_white_crop.png">
-        </div>
-        <div class="col s4">
-          <img class="z-depth-3 responsive-img hoverable" src="./img/metrics.png">
-        </div>
-        <div class="col s12">
-          <p class="light text-lighten-1">Brief description of Technologic.</p>
-        </div>
+<!--company section-->
+<div style="padding: 0 0 120px 0;">
+  <div style="padding: 0 10px 0 10px;">
+    <a id="about" name="about"></a>
+    <div class="row" style="margin-bottom: 0px;">
+      <div class="section">
+        <h4 class="center thin">ABOUT</h4>
       </div>
-    </div>
-
-    <div class="parallax-container">
-      <div class="parallax"><img src="./img/raccoon.jpg"></div>
-      <div class="slider">
-        <ul class="slides">
-          <li>
-            <img src="./img/Clear.png">
-            <div class="caption center-align">
-              <h5 class="shadow2 light">Customer review goes here. should be a pretty raving review. would help if it was a business owner or mayor.</h5>
-              <p>Andy Berke  - Mayor of Chattanooga</p>
+      <div class="col s4">
+        <img class="z-depth-3 responsive-img hoverable" src="./img/on_the_go.jpg">
+      </div>
+      <div class="col s4">
+        <img style="background-color: #7cc1f7;" class="z-depth-3 responsive-img hoverable" src="./img/Technologic_logo_white_crop.png">
+      </div>
+      <div class="col s4">
+        <img class="z-depth-3 responsive-img hoverable" src="./img/metrics.png">
+      </div>
+      <div class="col s12">
+        <div class="container">
+          <div class="section">
+            <div class="section">
+              <p class="light">Technologic is based in Chattanooga, where we enjoy the fastest internet in the country.</p>
+              <p class="light">We love our work and we know you will too.</p>
             </div>
-          </li>
-          <li>
-            <img src="./img/Clear.png">
-            <div class="caption center-align">
-              <h5 class="shadow2 light">Customer review goes here. should be a pretty raving review. would help if it was a business owner or mayor.</h5>
-            </div>
-          </li>
-          <li>
-            <img src="./img/Clear.png">
-            <div class="caption center-align">
-              <h5 class="shadow2 light">Customer review goes here. should be a pretty raving review. would help if it was a business owner or mayor.</h5>
-            </div>
-          </li>
-          <li>
-            <img src="./img/Clear.png">
-            <div class="caption center-align">
-              <h5 class="shadow2 light">Customer review goes here. should be a pretty raving review. would help if it was a business owner or mayor.</h5>
-            </div>
-          </li>
-          <li>
-            <img src="./img/Clear.png">
-            <div class="caption center-align">
-              <h5 class="shadow2 light">Customer review goes here. should be a pretty raving review. would help if it was a business owner or mayor.</h5>
-            </div>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </div>
-  <!--end company section-->
-
-
-  <!--team section-->
-  <div class="container" style="padding: 10px 0 50px 0;">
-    <a id="team" name="team"></a>
-    <div class="row">
-      <div class="section center">
-          <h4 class="center thin">TEAM</h4>
-      </div>
-      <div class="divider"></div>
-      <div style="padding: 15px 0;"></div> <!--maybe find a more elegant way to do this-->
-      <div class="col s12 xl6 center">
-        <div class="section">
-          <img class="z-depth-1 hoverable circle" style="height: 250px; width: 250px;" src="./img/gw_suit.png">
-          <p class="light">Greggory Wiley</p>
-          <em class="thin">owner / supreme leader</em>
-        </div>
-      </div>
-      <div class="col s12 xl6 center">
-        <div class="section">
-          <img class="z-depth-1 hoverable circle" style="height: 250px; width: 250px;" src="./img/Mallory_crop.png">
-          <p class="light">Mallory Nelson</p>
-          <em class="thin">graphic designer / advertising princess</em>
-        </div>
-      </div>
-      <div class="col s12 xl6 center">
-        <div class="section">
-          <img class="z-depth-1 hoverable circle" style="height: 250px; width: 250px;" src="./img/cole.jpg">
-          <p class="light">Cole Montalvo</p>
-          <em class="thin">developer / software breaker</em>
-        </div>
-      </div>
-      <div class="col s12 xl6 center">
-        <div class="section">
-          <img class="z-depth-1 hoverable circle" style="height: 250px; width: 250px;" src="./img/jaycie.jpg">
-          <p class="light">Jaycie Kelly</p>
-          <em class="thin">intern / minion</em>
+          </div>
         </div>
       </div>
     </div>
   </div>
-  <!--end team section-->
 
-
-  <div class="divider"></div>
-
-
-  <div class="col s12 m8">
-    <p>Making the transition to digital marketing can be intimidating. Thats where we come in. We've created a brief guide to help you understand the basic principles necessary for creating a strong, professional online presence.</p>
-    <button>Check it out</button>
-    <p>available though online search about digital marketing is convulted, conflicting, and aimed at getting you to sign up for any number of useless services which will waste time and money. In fact making the wrong choices can lead to various set backs in online presence and</p>
+  <div class="parallax-container">
+    <div class="parallax"><img src="./img/snoopers.jpg"></div>
+    <div class="slider">
+      <ul class="slides">
+        <li>
+          <img src="./img/Clear.png">
+          <div class="caption center-align">
+            <h5 class="shadow2 light">Customer review goes here. should be a pretty raving review. would help if it was a business owner or mayor.</h5>
+            <p>Andy Berke  - Mayor of Chattanooga</p>
+          </div>
+        </li>
+        <li>
+          <img src="./img/Clear.png">
+          <div class="caption center-align">
+            <h5 class="shadow2 light">Customer review goes here. should be a pretty raving review. would help if it was a business owner or mayor.</h5>
+          </div>
+        </li>
+        <li>
+          <img src="./img/Clear.png">
+          <div class="caption center-align">
+            <h5 class="shadow2 light">Customer review goes here. should be a pretty raving review. would help if it was a business owner or mayor.</h5>
+          </div>
+        </li>
+        <li>
+          <img src="./img/Clear.png">
+          <div class="caption center-align">
+            <h5 class="shadow2 light">Customer review goes here. should be a pretty raving review. would help if it was a business owner or mayor.</h5>
+          </div>
+        </li>
+        <li>
+          <img src="./img/Clear.png">
+          <div class="caption center-align">
+            <h5 class="shadow2 light">Customer review goes here. should be a pretty raving review. would help if it was a business owner or mayor.</h5>
+          </div>
+        </li>
+      </ul>
+    </div>
   </div>
+</div>
+<!--end company section-->
 
 
-  <div class="col s12">
-    <p>Meet with a Technologic team member and receive a free consultation on how your company can improve its online presence through online advertising. We will look over your companies analytics and discuss best practice strategies for your businesses and explain how we can partner with your business for digital success.</p>
+<!--team section-->
+<div class="container" style="padding: 10px 0 50px 0;">
+  <a id="team" name="team"></a>
+  <div class="row">
+    <div class="section center">
+      <h4 class="center thin">TEAM</h4>
+    </div>
+    <div class="divider"></div>
+    <div style="padding: 15px 0;"></div> <!--maybe find a more elegant way to do this-->
+    <div class="col s12 xl6 center">
+      <div class="section">
+        <img class="z-depth-1 hoverable circle" style="height: 250px; width: 250px;" src="./img/gw_suit.png">
+        <p class="light">Greggory Wiley</p>
+        <em class="thin">owner / supreme leader</em>
+      </div>
+    </div>
+    <div class="col s12 xl6 center">
+      <div class="section">
+        <img class="z-depth-1 hoverable circle" style="height: 250px; width: 250px;" src="./img/Mallory_crop.png">
+        <p class="light">Mallory Nelson</p>
+        <em class="thin">graphic designer / advertising princess</em>
+      </div>
+    </div>
+    <div class="col s12 xl6 center">
+      <div class="section">
+        <img class="z-depth-1 hoverable circle" style="height: 250px; width: 250px;" src="./img/cole.jpg">
+        <p class="light">Cole Montalvo</p>
+        <em class="thin">developer / software breaker</em>
+      </div>
+    </div>
+    <div class="col s12 xl6 center">
+      <div class="section">
+        <img class="z-depth-1 hoverable circle" style="height: 250px; width: 250px;" src="./img/jaycie.jpg">
+        <p class="light">Jaycie Kelly</p>
+        <em class="thin">intern / minion</em>
+      </div>
+    </div>
   </div>
-  <div class="row center section">
-    <a href="#contact-modal" id="download-button" class="modal-trigger btn-large waves-effect waves-light">Get In Touch</a>
-  </div>
+</div>
+<!--end team section-->
 
 
 
-  <?php include("footer.php"); ?>
+
+
+
+<?php include("footer.php"); ?>
