323d051f951d92b37db70104805cd5b9d01654b5
snewk <tehsnewk@me.com>
2017-08-15 13:22:17 -0400

    remove unused javascript initializations

--- js/init.js
+++ js/init.js
@@ -1,11 +1,8 @@
 (function($){
   $(function(){
-
-    $('.button-collapse').sideNav();
-    $('.parallax').parallax();
-
     $(document).ready(function () {
-
+      //$('.button-collapse').sideNav();
+      $('.parallax').parallax();
       $('.slider').slider({
         indicators: false,
         interval: 6000,
@@ -13,64 +10,24 @@
       });
 
 
-
-      //control nav scrolling
-      function scrollToAnchor(aid){
-        var aTag = $("a[name='"+ aid +"']");
-        $('html,body').animate({scrollTop: aTag.offset().top},'slow');
-      }
-
-      $("#scrollLink").click(function() {
-        scrollToAnchor('scrolldown');
-      })
-
-
-      //for google maps...
-      // you want to enable map pointer events only on click;
-
-      $('#map_canvas1').addClass('scrolloff'); // set the pointer events to none on doc ready
-      $('#canvas1').on('click', function () {
-        $('#map_canvas1').removeClass('scrolloff'); // set the pointer events true on click
-      });
-
-      // you want to disable map pointer events when the mouse leave the canvas area;
-
-      $("#map_canvas1").mouseleave(function () {
-        $('#map_canvas1').addClass('scrolloff'); // set the pointer events to none when mouse leaves the map area
-      });
-
     }); // end of document ready
+
+
 
 
     //control nav scrolling
     function scrollToAnchor(aid){
       var aTag = $("a[name='"+ aid +"']");
-      $('html,body').animate({scrollTop: aTag.offset().top - 60},'slow');
+      $('html,body').animate({scrollTop: aTag.offset().top},'slow');
     }
 
-    $("#logo-container").click(function() {
-      //  scrollToAnchor('services');
-    });
-
-    $("#scrollLink_services").click(function() {
+    $("#scrollLink").click(function() {
       scrollToAnchor('services');
-    });
+    })
 
-    $("#scrollLink_testimonials").click(function() {
-      scrollToAnchor('testimonials');
-    });
-
-    $("#scrollLink_location").click(function() {
-      scrollToAnchor('location');
-    });
-
-    $("#scrollLink_contact").click(function() {
-      scrollToAnchor('contact');
-    });
-
-    $("#scrollLink_about").click(function() {
-      scrollToAnchor('about');
-    });
+    $('#scrollLink_services').click(function() {
+    scrollToAnchor('services');
+    })
 
     //control sideNav scrolling
 
@@ -81,25 +38,11 @@
       scrollToAnchor('services');
     });
 
-    $('#scrollLink_sidetestimonials').click(function() {
+    $('#scrollLink_sidework').click(function() {
       $('#sidenav-overlay').trigger('click');
     })
-    $("#scrollLink_sidetestimonials").click(function() {
-      scrollToAnchor('testimonials');
-    });
-
-    $('#scrollLink_sidelocation').click(function() {
-      $('#sidenav-overlay').trigger('click');
-    })
-    $("#scrollLink_sidelocation").click(function() {
-      scrollToAnchor('location');
-    });
-
-    $('#scrollLink_sidecontact').click(function() {
-      $('#sidenav-overlay').trigger('click');
-    })
-    $("#scrollLink_sidecontact").click(function() {
-      scrollToAnchor('contact');
+    $("#scrollLink_sidework").click(function() {
+      scrollToAnchor('work');
     });
 
     $('#scrollLink_sideabout').click(function() {
@@ -107,6 +50,13 @@
     })
     $("#scrollLink_sideabout").click(function() {
       scrollToAnchor('about');
+    });
+
+    $('#scrollLink_sidefooter').click(function() {
+      $('#sidenav-overlay').trigger('click');
+    })
+    $("#scrollLink_sidefooter").click(function() {
+      scrollToAnchor('footer');
     });
 
 
