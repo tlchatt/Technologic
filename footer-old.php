104098d89dacc43b6fc1750f03c66c342355fc8e
snewk <tehsnewk@me.com>
2017-08-15 13:22:38 -0400

    save some content for later maybe

---
+++ footer-old.php
@@ -0,0 +1,60 @@
+.ionicons {
+  @font-face {
+      font-family: "ionicons";
+      src: url("./fonts/ionicons.woff") format('woff');
+  }
+}
+
+
+
+
+<div class="divider"></div>
+
+
+<div class="col s12 m8">
+  <p>Making the transition to digital marketing can be intimidating. Thats where we come in. We've created a brief guide to help you understand the basic principles necessary for creating a strong, professional online presence.</p>
+  <button>Check it out</button>
+  <p>available though online search about digital marketing is convulted, conflicting, and aimed at getting you to sign up for any number of useless services which will waste time and money. In fact making the wrong choices can lead to various set backs in online presence and</p>
+  <p>We are on a mission to bring value to our clients, helping them to achieve success in an online world. We make advanced digital marketing practical and accessible to our clients. </p>
+</div>
+
+
+<div class="col s12">
+  <p>Meet with a Technologic team member and receive a free consultation on how your company can improve its online presence through online advertising. We will look over your companies analytics and discuss best practice strategies for your businesses and explain how we can partner with your business for digital success.</p>
+</div>
+<div class="row center section">
+  <a href="#contact-modal" id="download-button" class="modal-trigger btn-large waves-effect waves-light">Get In Touch</a>
+</div>
+
+
+<div class="">
+  <h5 class="white-text light"><?php echo"$content $date2";?></h5>
+  <p class="grey-text text-lighten-4 light ">We are currently in the office. Feel free to call us with questions. We would love to hear from you!</p>
+</div>
+
+
+<div class="">
+  <h4 class=" text-darken-3">Reach Audiences Aross All Platforms</h4>
+  <ul>
+    <li><i class="icon white-text" href="http://tlchatt.com/google-adwords-advertising-management">ion-social-google</i></li>
+    <li><i class="icon white-text" href="http://tlchatt.com/facebook-advertising-management">ion-social-facebook</i></li>
+    <li><i class="icon white-text" href="http://tlchatt.com/#">ion-social-twitter</i></li>
+    <li><i class="icon white-text" href="http://tlchatt.com/#">ion-social-instagram-outline</i></li>
+    <li><i class="icon white-text" href="http://tlchatt.com/#">ion-social-pinterest</i></li>
+    <li><i class="icon white-text" href="http://tlchatt.com/#">ion-social-youtube-outline</i></li>
+  </ul>
+</div>
+<div class="">
+  <h5 class="white-text">Search Engine and Social Optimization</h5>
+  <ul>
+    <li><a class="white-text" href="#!">Website Basics</a></li>
+    <li><a class="white-text" href="#!">Social Signals</a></li>
+    <li><a class="white-text" href="#!">Structured Data</a></li>
+  </ul>
+</div>
+
+<div class="footer-copyright">
+  <div class="container center">
+    <a class="grey-text text-darken-1" href="http://tlchatt.com">© 2017 Technologic, Inc. All Rights Reserved</a>
+  </div>
+</div>
