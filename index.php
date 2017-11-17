96c16edf0dd16fdb8d96d9cd8f99c347638e5b25
snewk <tehsnewk@me.com>
2017-11-17 14:50:54 -0500

    replace images on homepage and adjust  columns for XXL screen widths

--- index.php
+++ index.php
@@ -2,32 +2,44 @@
 
 <!--fullscreen slider intro image-->
 <div class="content">
-<div class="parallax-container fullscreen">
-  <div class="slider fullscreen">
-    <ul class="slides">
-      <li><img src="./img/Optimized-walnut_st_bridge_filter_cropped.jpg"></li>
-      <li><img src="./img/raccoon.jpg"></li>
-      <li><img src="./img/rooftop.jpg"></li>
-      <li><img src="./img/aquarium.jpg"></li>
-      <li><img src="./img/dam.jpg"></li>
-      <li><img src="./img/miller.jpg"></li>
-    </ul>
-    <div>
-      <div class="caption-wrapper left-align">
-        <h1 class="shadow2 light heading">Web Design, SEO, & Digital Marketing for the Modern Web</h1>
-        <h5 class="light text-lighten-3 shadow">We deliver real results for an unreal price.</h5>
-      </div>
+  <div class="parallax-container fullscreen">
+    <div class="slider fullscreen">
+      <ul class="slides">
+        <li>
+          <img src="./img/Optimized-walnut_st_bridge_filter_cropped.jpg">
+        </li>
+        <li>
+          <img src="./img/raccoon.jpg">
+        </li>
+        <li>
+          <img src="./img/rooftop.jpg">
+        </li>
+        <li>
+          <img src="./img/aquarium.jpg">
+        </li>
+        <li>
+          <img src="./img/dam.jpg">
+        </li>
+        <li>
+          <img src="./img/miller.jpg">
+        </li>
+      </ul>
+      <div>
+        <div class="caption-wrapper left-align">
+          <h1 class="shadow2 light heading">Web Design, SEO, & Digital Marketing for the Modern Web</h1>
+          <h5 class="light text-lighten-3 shadow">We deliver real results for an unreal price.</h5>
+        </div>
 
-      <div class="logo-wrapper">
-        <img src="./img/technologic_square_white_logo.png">
-      </div>
+        <div class="logo-wrapper">
+          <img src="./img/technologic_square_white_logo.png">
+        </div>
 
-      <div id="scrollLink" href="#" class="scroll-wrapper">
-        <i class="fa fa-angle-down hoverable-flat shadow2" aria-hidden="true"></i>
+        <div id="scrollLink" href="#" class="scroll-wrapper">
+          <i class="fa fa-angle-down hoverable-flat shadow2" aria-hidden="true"></i>
+        </div>
       </div>
     </div>
   </div>
-</div>
 </div>
 <!--end of fullscreen slider-->
 
@@ -48,35 +60,41 @@ end of sticky nav -->
 <!--services section-->
 <div class="after-nav" style="padding: 70px 0px 70px 0px;">
   <a id="services" name="services"></a>
-    <h4 id="servicesTitle" class="center thin invisible">SERVICES</h4>
-    <div id="servicesDiv" class="invisible divider"></div>
-    <div class="container" style="padding: 35px 0px 35px 0px;">
-      
-      <div class="row">
-        <div id="servicesOffering1" class="invisible col s12 l6">
-          <h5 class="light"><i class="fa fa-sitemap" aria-hidden="true" style="padding-right: 1rem;"></i>WEB DESIGN & DEVELOPMENT</h5>
-          <p class="light text-lighten-1">Custom built, professional, and user friendly website design. Created with search engine ranking and social media sharing in mind. </p>
-        </div>
-        <div id="servicesOffering2" class="invisible col s12 l6">
-          <h5 class="light"><i class="fa fa-line-chart" aria-hidden="true" style="padding-right: 1rem;"></i>SEARCH ENGINE OPTIMIZATION</h5>
-          <p class="light text-lighten-1">Rank highly and look professional on search. Enough said.</p>
-        </div>
-      </div>
+  <h4 id="servicesTitle" class="center thin invisible">SERVICES</h4>
+  <div id="servicesDiv" class="invisible divider"></div>
+  <div class="container" style="padding: 35px 0px 35px 0px;">
 
-      <div class="row">
-        <div id="servicesOffering3" class="invisible col s12 l6">
-          <h5 class="light"><i class="fa fa-bullhorn" aria-hidden="true" style="padding-right: 1rem;"></i>ONLINE ADVERTISING</h5>
-          <p class="light text-lighten-1">Reach your ideal customer wherever they spend time online. Advertise across search and social platforms. Our strategies ensure the best possible ROI for your budget.</p>
-        </div>
-        <div id="servicesOffering4" class="invisible col s12 l6">
-          <h5 class="light"><i class="fa fa-shopping-cart" aria-hidden="true" style="padding-right: 1rem;"></i>E-COMMERCE MARKETING</h5>
-
-          <p class="light text-lighten-1">E-Commerce specific digital advertsing management on Goole Shopping, Facebook, Instagram, Pinterest and more.</p>
-        </div>
+    <div class="row">
+      <div id="servicesOffering1" class="invisible col s12 l6">
+        <h5 class="light">
+          <i class="fa fa-sitemap" aria-hidden="true" style="padding-right: 1rem;"></i>WEB DESIGN & DEVELOPMENT</h5>
+        <p class="light text-lighten-1">Custom built, professional, and user friendly website design. Created with search engine ranking and social media
+          sharing in mind. </p>
       </div>
-    
+      <div id="servicesOffering2" class="invisible col s12 l6">
+        <h5 class="light">
+          <i class="fa fa-line-chart" aria-hidden="true" style="padding-right: 1rem;"></i>SEARCH ENGINE OPTIMIZATION</h5>
+        <p class="light text-lighten-1">Rank highly and look professional on search. Enough said.</p>
+      </div>
     </div>
+
+    <div class="row">
+      <div id="servicesOffering3" class="invisible col s12 l6">
+        <h5 class="light">
+          <i class="fa fa-bullhorn" aria-hidden="true" style="padding-right: 1rem;"></i>ONLINE ADVERTISING</h5>
+        <p class="light text-lighten-1">Reach your ideal customer wherever they spend time online. Advertise across search and social platforms. Our strategies
+          ensure the best possible ROI for your budget.</p>
+      </div>
+      <div id="servicesOffering4" class="invisible col s12 l6">
+        <h5 class="light">
+          <i class="fa fa-shopping-cart" aria-hidden="true" style="padding-right: 1rem;"></i>E-COMMERCE MARKETING</h5>
+
+        <p class="light text-lighten-1">E-Commerce specific digital advertsing management on Goole Shopping, Facebook, Instagram, Pinterest and more.</p>
+      </div>
+    </div>
+
   </div>
+</div>
 </div>
 
 
@@ -85,21 +103,21 @@ end of sticky nav -->
 <!--work section-->
 <div style="background-color: #f7f7f7; padding: 0 10px 120px 10px;">
   <a id="work" name="work"></a>
-  <div class="row">
+  <div class="row center">
     <div class="section">
       <h4 id="workTitle" class="invisible center thin">WORK</h4>
     </div>
-    <div id="workImg1" class="invisible col s6" style="padding: 0 10px 15px 10px;">
-      <img class="z-depth-3 responsive-img hoverable" src="./img/blueplate.jpg">
+    <div id="workImg1" class="invisible col s6 m3" style="padding: 0 10px 15px 10px;">
+      <img class="z-depth-3 responsive-img hoverable" src="./img/blueplate.png">
     </div>
-    <div id="workImg2" class="invisible col s6" style="padding: 0 10px 15px 10px;">
-      <img class="z-depth-3 responsive-img hoverable" src="./img/johnsen.jpg">
+    <div id="workImg2" class="invisible col s6 m3" style="padding: 0 10px 15px 10px;">
+      <img class="z-depth-3 responsive-img hoverable" src="./img/johnsen.png">
     </div>
-    <div id="workImg3" class="invisible col s6" style="padding: 0 10px 15px 10px;">
-      <img class="z-depth-3 responsive-img hoverable" src="./img/chattown.jpg">
+    <div id="workImg3" class="invisible col s6 m3" style="padding: 0 10px 15px 10px;">
+      <img class="z-depth-3 responsive-img hoverable" src="./img/chattown.png">
     </div>
-    <div id="workImg4" class="invisible col s6" style="padding: 0 10px 15px 10px;">
-      <img class="z-depth-3 responsive-img hoverable" src="./img/tristate.jpg">
+    <div id="workImg4" class="invisible col s6 m3" style="padding: 0 10px 15px 10px;">
+      <img class="z-depth-3 responsive-img hoverable" src="./img/tristate.png">
     </div>
   </div>
 </div>
@@ -114,14 +132,16 @@ end of sticky nav -->
       <div class="section">
         <h4 id="aboutTitle" class="invisible center thin">ABOUT US</h4>
       </div>
-      <div id="aboutImg1" class="invisible col s4">
-        <img class="z-depth-3 responsive-img hoverable" src="./img/on_the_go.jpg">
-      </div>
-      <div id="aboutImg2" class="invisible col s4">
-        <img style="background-color: #7cc1f7;" class="z-depth-3 responsive-img hoverable" src="./img/Technologic_logo_white_crop.png">
-      </div>
-      <div id="aboutImg3" class="invisible col s4">
-        <img class="z-depth-3 responsive-img hoverable" src="./img/metrics.jpg">
+      <div class="container">
+        <div id="aboutImg1" class="invisible col s4">
+          <img class="z-depth-3 responsive-img hoverable" src="./img/on_the_go.jpg">
+        </div>
+        <div id="aboutImg2" class="invisible col s4">
+          <img style="background-color: #7cc1f7;" class="z-depth-3 responsive-img hoverable" src="./img/Technologic_logo_white_crop.png">
+        </div>
+        <div id="aboutImg3" class="invisible col s4">
+          <img class="z-depth-3 responsive-img hoverable" src="./img/metrics.jpg">
+        </div>
       </div>
       <div class="col s12">
         <div class="container">
@@ -137,41 +157,51 @@ end of sticky nav -->
   </div>
 
   <div class="parallax-container">
-    <div class="parallax"><img src="./img/snoopers.jpg"></div>
+    <div class="parallax">
+      <img src="./img/snoopers.jpg">
+    </div>
     <div class="slider">
       <ul class="slides">
         <li>
           <img src="./img/Clear.png">
           <div class="caption responsive-caption center-align">
-            <h6 class="shadow2 light">Technologic is the most hands-on online marketing company in the Chattanooga area. Professional services with measurable results. They've helped us build a top-notch web presence from scratch and achieve a noticeable increases in sales.</h6>
+            <h6 class="shadow2 light">Technologic is the most hands-on online marketing company in the Chattanooga area. Professional services with
+              measurable results. They've helped us build a top-notch web presence from scratch and achieve a noticeable
+              increases in sales.</h6>
             <p>- Scott V.</p>
           </div>
         </li>
         <li>
           <img src="./img/Clear.png">
           <div class="caption responsive-caption center-align">
-            <h6 class="shadow2 light">I've worked with Greggory for the past couple of years and I would say he is one of the most knowledgeable in the Chattanooga area! I would highly recommend him for your website and SEO needs.</h6>
+            <h6 class="shadow2 light">I've worked with Greggory for the past couple of years and I would say he is one of the most knowledgeable in
+              the Chattanooga area! I would highly recommend him for your website and SEO needs.</h6>
             <p>- Martin M.</p>
           </div>
         </li>
         <li>
           <img src="./img/Clear.png">
           <div class="caption responsive-caption center-align">
-            <h6 class="shadow2 light">They are the absolute best at what they do. They designed my website and do my adwords. Gregg is the man. He is very honest, and doesn't sell anything you don't need. I chose Technologic and am very glad I did. Highly recommend!</h6>
+            <h6 class="shadow2 light">They are the absolute best at what they do. They designed my website and do my adwords. Gregg is the man. He
+              is very honest, and doesn't sell anything you don't need. I chose Technologic and am very glad I did. Highly
+              recommend!</h6>
             <p>- Josh W.</p>
           </div>
         </li>
         <li>
           <img src="./img/Clear.png">
           <div class="caption responsive-caption center-align">
-            <h6 class="shadow2 light">They literally do it all, and they do it better than anyone. If you're looking for a place that does web design, adwords, or search engine optimization, call Gregg.</h6>
+            <h6 class="shadow2 light">They literally do it all, and they do it better than anyone. If you're looking for a place that does web design,
+              adwords, or search engine optimization, call Gregg.</h6>
             <p>- Katie A.</p>
           </div>
         </li>
         <li>
           <img src="./img/Clear.png">
           <div class="caption responsive-caption center-align">
-            <h6 class="shadow2 light">Gregg and his Technologic team have consistently helped my Chiropractic office stay relevant online for three years running. They built and maintain my site which looks wonderful and stays in the top three positions on search. They also run  highly effective advertising on Facebook.</h6>
+            <h6 class="shadow2 light">Gregg and his Technologic team have consistently helped my Chiropractic office stay relevant online for three
+              years running. They built and maintain my site which looks wonderful and stays in the top three positions on
+              search. They also run highly effective advertising on Facebook.</h6>
             <p>- Denise W.</p>
           </div>
         </li>
@@ -190,7 +220,8 @@ end of sticky nav -->
       <h4 id="teamTitle" class="invisible center thin">TEAM</h4>
     </div>
     <div id="teamDiv" class="invisible divider"></div>
-    <div style="padding: 15px 0;"></div> <!--maybe find a more elegant way to do this-->
+    <div style="padding: 15px 0;"></div>
+    <!--maybe find a more elegant way to do this-->
     <div id="team1" class="invisible col s12 xl6 center">
       <div class="section">
         <img class="z-depth-1 hoverable circle" style="height: 250px; width: 250px;" src="./img/gw_suit.jpg">
